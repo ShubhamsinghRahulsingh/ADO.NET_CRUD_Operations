957fd31272fc3dd8c55330077f904c78a6ee9767
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-12-27 00:30:06 +0530

    (Shubham)Add: Project Folder Structure Created

---
+++ CRUD_StoredProcedures/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CRUD_StoredProcedures
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the CRUD Operations Using Stored Procedures");
+        }
+    }
+}
